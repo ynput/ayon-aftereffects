e19df39aa5ebe80a8bb310589aaaa1c435288573
Roy Nieterau <roy_nieterau@hotmail.com>
2024-10-07 22:58:33 +0200

    Define app host name

--- package.py
+++ package.py
@@ -1,9 +1,10 @@
 name = "aftereffects"
 title = "AfterEffects"
 version = "0.2.4-dev.1"
-
+app_host_name = "aftereffects"
 client_dir = "ayon_aftereffects"
 
+ayon_server_version = ">=1.1.2"
 ayon_required_addons = {
     "core": ">0.3.2",
 }
