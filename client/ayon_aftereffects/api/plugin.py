28d5a5930bb8888b78b185cfebfcdf2911e6e343
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-02-10 17:33:04 +0100

    remove usage of 'six'

--- client/ayon_aftereffects/api/plugin.py
+++ client/ayon_aftereffects/api/plugin.py
@@ -1,11 +1,8 @@
-import six
-from abc import ABCMeta
-
 from ayon_core.pipeline import LoaderPlugin
+
 from .launch_logic import get_stub
 
 
-@six.add_metaclass(ABCMeta)
 class AfterEffectsLoader(LoaderPlugin):
     @staticmethod
     def get_stub():
