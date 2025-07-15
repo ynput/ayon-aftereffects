5a9d91aa0c1723418535ada8c76ae7f6ea2dd065
Anh Tu <tounh17@gmail.com>
2025-07-16 06:18:05 +1000

    use self.lanch_context.env instead of os.environ
    use self.lanch_context.env instead of os.environ to pass env variable to the launch_logic
    
    Co-authored-by: Petr Kalis <petr.kalis@gmail.com>

--- client/ayon_aftereffects/hooks/pre_launch_args.py
+++ client/ayon_aftereffects/hooks/pre_launch_args.py
@@ -79,7 +79,7 @@ class AEPrelaunchHook(PreLaunchHook):
 
         workfile_startup = self.data.get("workfile_startup", False)
         # set the env variable AYON_AFTEREFFECTS_WORKFILES_ON_LAUNCH to "true" or "false"
-        os.environ["AYON_AFTEREFFECTS_WORKFILES_ON_LAUNCH"] = str(workfile_startup).lower()
+        self.launch_context.env["AYON_AFTEREFFECTS_WORKFILES_ON_LAUNCH"] = str(workfile_startup).lower()
 
         # Append as whole list as these arguments should not be separated
         self.launch_context.launch_args.append(new_launch_args)
