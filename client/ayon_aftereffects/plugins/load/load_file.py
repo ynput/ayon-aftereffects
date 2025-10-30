bc8c56ffb63509486670d05dd61714bd58058d6c
Petr Kalis <petr.kalis@gmail.com>
2025-10-30 18:11:51 +0100

    Refactored _get_unique_loaded_item_name to use more generic

--- client/ayon_aftereffects/plugins/load/load_file.py
+++ client/ayon_aftereffects/plugins/load/load_file.py
@@ -3,7 +3,6 @@ import os
 
 from ayon_core.pipeline import get_representation_path
 from ayon_aftereffects import api
-from ayon_aftereffects.api.lib import get_unique_item_name
 
 
 class FileLoader(api.AfterEffectsLoader):
@@ -26,8 +25,9 @@ class FileLoader(api.AfterEffectsLoader):
     def load(self, context, name=None, namespace=None, data=None):
         stub = self.get_stub()
         loaded_item_name = f"{context['folder']['name']}_{name}"
+        footages = stub.get_items(comps=False, footages=True, folders=False)
         loaded_item_name = self._get_unique_loaded_item_name(
-            stub, loaded_item_name
+            stub, footages, loaded_item_name
         )
 
         import_options = {}
@@ -82,8 +82,11 @@ class FileLoader(api.AfterEffectsLoader):
 
         loaded_item_name = f"{folder_name}_{product_name}"
         if namespace_from_container != loaded_item_name:
+            footages = stub.get_items(
+                comps=False, footages=True, folders=False
+            )
             loaded_item_name = self._get_unique_loaded_item_name(
-                stub, loaded_item_name
+                stub, footages, loaded_item_name
             )
         else:  # switching version - keep same name
             loaded_item_name = container["namespace"]
@@ -101,26 +104,3 @@ class FileLoader(api.AfterEffectsLoader):
             }
         )
 
-    def remove(self, container):
-        """
-            Removes element from scene: deletes layer + removes from Headline
-        Args:
-            container (dict): container to be removed - used to get layer_id
-        """
-        stub = self.get_stub()
-        item = container.pop("layer")
-        stub.imprint(item.id, {})
-        stub.delete_item(item.id)
-
-    def switch(self, container, context):
-        self.update(container, context)
-
-    def _get_unique_loaded_item_name(self, stub, loaded_item_name):
-        footages = stub.get_items(comps=False, footages=True, folders=False)
-        existing_footage_names = [
-            footage.name.replace(stub.LOADED_ICON, "") for footage in footages
-        ]
-        loaded_item_name = get_unique_item_name(
-            existing_footage_names, loaded_item_name
-        )
-        return loaded_item_name
