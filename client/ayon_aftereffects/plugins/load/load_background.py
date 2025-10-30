2c626bf2d80ba6fe31a247eb59199717620c9496
Petr Kalis <petr.kalis@gmail.com>
2025-10-30 18:12:26 +0100

    Refactored background loader
    No real background products in AYON right now though.

--- client/ayon_aftereffects/plugins/load/load_background.py
+++ client/ayon_aftereffects/plugins/load/load_background.py
@@ -3,10 +3,7 @@ import re
 from ayon_core.pipeline import get_representation_path
 
 from ayon_aftereffects import api
-from ayon_aftereffects.api.lib import (
-    get_background_layers,
-    get_unique_item_name,
-)
+from ayon_aftereffects.api.lib import get_background_layers
 
 
 class BackgroundLoader(api.AfterEffectsLoader):
@@ -25,33 +22,34 @@ class BackgroundLoader(api.AfterEffectsLoader):
 
     def load(self, context, name=None, namespace=None, data=None):
         stub = self.get_stub()
-        items = stub.get_items(comps=True)
-        existing_items = [layer.name.replace(stub.LOADED_ICON, '')
-                          for layer in items]
-
-        comp_name = get_unique_item_name(
-            existing_items,
-            "{}_{}".format(context["folder"]["name"], name))
+        loaded_item_name = f"{context['folder']['name']}_{name}"
+        comps = stub.get_items(comps=True)
+        loaded_item_name = self._get_unique_loaded_item_name(
+            stub, comps, loaded_item_name
+        )
 
         path = self.filepath_from_context(context)
         layers = get_background_layers(path)
         if not layers:
             raise ValueError("No layers found in {}".format(path))
 
-        comp = stub.import_background(None, stub.LOADED_ICON + comp_name,
-                                      layers)
+        loaded_item = stub.import_background(
+            None, stub.LOADED_ICON + loaded_item_name, layers
+        )
 
-        if not comp:
-            raise ValueError("Import background failed. "
-                             "Please contact support")
+        if not loaded_item:
+            raise ValueError(
+                "Import background failed. "
+                "Please contact support"
+            )
 
-        self[:] = [comp]
-        namespace = namespace or comp_name
+        self[:] = [loaded_item]
+        namespace = namespace or loaded_item
 
         return api.containerise(
             name,
             namespace,
-            comp,
+            loaded_item,
             context,
             self.__class__.__name__
         )
@@ -65,46 +63,31 @@ class BackgroundLoader(api.AfterEffectsLoader):
         _ = container.pop("layer")
 
         # without iterator number (_001, 002...)
-        namespace_from_container = re.sub(r'_\d{3}$', '',
-                                          container["namespace"])
-        comp_name = "{}_{}".format(folder_name, product_name)
+        namespace_from_container = re.sub(
+            r"_\d{3}$", "", container["namespace"]
+        )
 
+        loaded_item_name = f"{folder_name}_{product_name}"
         # switching assets
-        if namespace_from_container != comp_name:
-            items = stub.get_items(comps=True)
-            existing_items = [layer.name for layer in items]
-            comp_name = get_unique_item_name(
-                existing_items,
-                "{}_{}".format(folder_name, product_name))
+        if namespace_from_container != loaded_item_name:
+            loaded_item_name = self._get_unique_loaded_item_name(
+                stub, loaded_item_name)
         else:  # switching version - keep same name
-            comp_name = container["namespace"]
+            loaded_item_name = container["namespace"]
 
         path = get_representation_path(repre_entity)
 
         layers = get_background_layers(path)
-        comp = stub.reload_background(container["members"][1],
-                                      stub.LOADED_ICON + comp_name,
-                                      layers)
+        loaded_item = stub.reload_background(
+            container["members"][1],
+            stub.LOADED_ICON + loaded_item_name,
+            layers
+        )
 
         # update container
         container["representation"] = repre_entity["id"]
         container["name"] = product_name
-        container["namespace"] = comp_name
-        container["members"] = comp.members
+        container["namespace"] = loaded_item_name
+        container["members"] = loaded_item.members
 
-        stub.imprint(comp.id, container)
-
-    def remove(self, container):
-        """
-            Removes element from scene: deletes layer + removes from file
-            metadata.
-        Args:
-            container (dict): container to be removed - used to get layer_id
-        """
-        stub = self.get_stub()
-        layer = container.pop("layer")
-        stub.imprint(layer.id, {})
-        stub.delete_item(layer.id)
-
-    def switch(self, container, context):
-        self.update(container, context)
+        stub.imprint(loaded_item.id, container)
